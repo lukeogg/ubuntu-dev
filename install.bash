95d362dbd926a4921ab85a9432c8e2773b33092e
lukeogg <logg@d2iq.com>
2021-12-15 18:06:31 -0700

    strip down the install script to the min

--- install.bash
+++ install.bash
@@ -5,48 +5,35 @@ set -euo pipefail
 apt-get update
 
 # not sure of we need this
-apt-get install -y software-properties-common
+#apt-get install -y software-properties-common
 
 apt-get install -y \
-  build-essential \
-  ca-certificates \
-  checkinstall \
   curl \
-  libcurl4 \
   git \
   gnupg2 \
   make \
-  libssl-dev \
-  libncursesw5-dev \
-  libssl-dev \
-  libsqlite3-dev \
-  libgdbm-dev \
-  libc6-dev  \
-  libbz2-dev \
-  liblz4-tool \
-  lsb-release \
-  lzop \
   pbzip2 \
-  python-dev \
   python3 \
-  python3-dev \
   python3-pip \
   python3-venv \
-  remake \
-  rpm \
-  ruby-dev \
-  tk-dev \
   tar \
   unzip
 
 
-# Docker 
+# Docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+apt-get update
 apt-get install -y \
   docker-ce \
   docker-ce-cli \
