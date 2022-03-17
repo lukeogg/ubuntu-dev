a4adb96fa3ba34c5378236425fba79667eeeeb5e
lukeogg <logg@d2iq.com>
2022-03-17 10:12:32 -0600

    Add connect script

---
+++ connect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Get current cer file from inventory file
+cert_path=$(cat inventory | ggrep -Po "(?<==).*\.pem")
+
+#Get Host from inventory file
+host=$(cat inventory | ggrep -Po "(.*)amazonaws\.com")
+
+ssh -i $cert_path ubuntu@$host
