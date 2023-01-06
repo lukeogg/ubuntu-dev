5ebcbe9c98862d652e532249bc9dfcbbb3f2b69b
Luke Ogg <logg@d2iq.com>
2023-01-06 14:16:28 -0700

    Update port forwards (not working yet)

--- code.sh
+++ code.sh
@@ -19,7 +19,8 @@ echo "Host $host" >> ~/.ssh/config
 echo "    User ubuntu" >> ~/.ssh/config
 echo "    HostName $host" >> ~/.ssh/config
 echo "    IdentityFile $PWD/$cert_path" >> ~/.ssh/config
-#echo "    LocalForward 127.0.0.1:27017 127.0.0.1:27017" >> ~/.ssh/config
+#echo "    LocalForward 127.0.0.1:8080 172.18.255.200:443" >> ~/.ssh/config
+#echo "    LocalForward 127.0.0.1:1337 127.0.0.1:1337" >> ~/.ssh/config
 
 # This script is used to open a remote VSCode session.
 #code --folder-uri=vscode-remote://$host/home/go/src/github.com/mesosphere/dkp-insights/
