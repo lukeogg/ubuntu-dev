b04f824152385a23960d14cb1868d0cfd5492c14
Luke Ogg <logg@d2iq.com>
2023-08-30 08:43:26 -0600

    Bump Kind to v0.20.0

--- install.bash
+++ install.bash
@@ -55,7 +55,7 @@ chmod +x ./clusterctl
 mv ./clusterctl /usr/local/bin/clusterctl
 
 # Kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
